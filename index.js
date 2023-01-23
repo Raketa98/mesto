41af9e0173e01b5ee9878eb5b7cdc654e01b04d8
Damir Gazizov <dam-gaz@yandex.ru>
2023-01-23 18:25:21 +0300

    карточки, форма добавления, лайки

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// блок редактирования
 const popupBlock = document.querySelector('.popup');
 const btnEdit = document.querySelector('.profile__edit-btn');
 const userName = document.querySelector('.profile__name');
@@ -5,19 +6,32 @@ const userDesc = document.querySelector('.profile__desc');
 const btnClose = document.querySelector('.popup__close');
 const inputName = document.querySelector('.popup__input_type_name');
 const inputDesc = document.querySelector('.popup__input_type_desc');
+// блок добаления
+const popupBlockAdd = document.querySelector('.popup-add');
+const btnAdd = document.querySelector('.profile__add-btn');
+const placeTitle = document.querySelector('.elements__title');
+const placeLink = document.querySelector('.elements__img');
+const btnAddClose = document.querySelector('.popup-add__close');
+const inputPlace = document.querySelector('.popup-add__input_type_place');
+const inputLink = document.querySelector('.popup-add__input_type_link');
+// формы
 const formElement = document.querySelector('.popup__form');
+const formAddElement = document.querySelector('.popup-add__form');
+// контейнер для шаблона
+const elementsContainer = document.querySelector('.elements__list');
 
-const openPopup = function() {
+function openPopup(e) {
+  e.preventDefault();
   popupBlock.classList.add('popup_opened');
   inputName.value = userName.textContent;
   inputDesc.value = userDesc.textContent;
 }
-btnEdit.addEventListener('click', openPopup)
+btnEdit.addEventListener('click', openPopup);
 
-const closePopup = function() {
+function closePopup() {
   popupBlock.classList.remove('popup_opened');
 }
-btnClose.addEventListener('click', closePopup)
+btnClose.addEventListener('click', closePopup);
 
 function handleFormSubmit(e) {
   e.preventDefault;
@@ -27,5 +41,62 @@ function handleFormSubmit(e) {
 
   closePopup();
 }
-
 formElement.addEventListener('submit', handleFormSubmit);
+
+function openPopupAdd(e) {
+  e.preventDefault();
+  popupBlockAdd.classList.add('popup-add_opened');
+}
+btnAdd.addEventListener('click', openPopupAdd);
+
+function closePopupAdd() {
+  popupBlockAdd.classList.remove('popup-add_opened');
+}
+btnAddClose.addEventListener('click', closePopupAdd);
+
+// массив изображений, создание карточки по шаблону и отображение на странице
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+function createCards(val) {
+  const elementsTemplate = document.querySelector('#elements-template').content;
+  const cardElement = elementsTemplate.querySelector('.elements__item').cloneNode(true);
+
+  cardElement.querySelector('.elements__img').src = val.link;
+  cardElement.querySelector('.elements__img').alt = val.name;
+  cardElement.querySelector('.elements__title').textContent = val.name;
+
+  cardElement.querySelector('.elements__like-btn').addEventListener('click', function(e) {
+    e.target.classList.toggle('elements__like-btn_active');
+  })
+
+  return cardElement;
+}
+
+initialCards.forEach(el => {
+  elementsContainer.prepend(createCards(el));
+})
